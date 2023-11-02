9c37e0876bff347b838ccad2b53049e2c90a855f
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-11-02 15:12:40 +0900

    [Add] match login and password

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,28 @@ import com.example.demo.form.LoginForm;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+	
+	private static final String LOGIN_ID="user";
+	private static final String PASSWORD="pass";
+	
 	@GetMapping()
-	public String view(LoginForm form) {
+	public String view(Model model, LoginForm form) {
 		return "login";
 	}
 	
 	@PostMapping()
-	public void login(LoginForm form) {
+	public String login(Model model, LoginForm form) {
 		System.out.println(form.toString());
+		var isCorrectLoginId = form.getLoginId().equals(LOGIN_ID);
+		var isCorrectPassword = form.getPassword().equals(PASSWORD);
+		System.out.println(isCorrectLoginId);
+		System.out.println(isCorrectPassword);
+		var isCorrectUserAuth = isCorrectLoginId && isCorrectPassword;
+		if(isCorrectUserAuth) {
+			return "redirect:/mypage";
+		} else {
+			model.addAttribute("msg", "ログイン情報が一致しません");
+			return "login";
+		}
 	}
 }
