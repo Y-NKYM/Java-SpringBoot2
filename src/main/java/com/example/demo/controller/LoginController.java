8e0220cb5efd850dc07e2ffaf09574c540f0a2fc
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-11-03 17:40:55 +0900

    [Add] authenticate using hashed password

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,28 +17,43 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/login")
 public class LoginController {
 	
-//	private static final String LOGIN_ID="user";
-//	private static final String PASSWORD="pass";
+	/** ログイン画面 Service */
 	private final LoginService service;
 	
+	/** PasswordEncoder */
+	private final PasswordEncoder passwordEncoder;
+	
+	/**
+	 * ログイン画面
+	 * 
+	 * @param model
+	 * @param form
+	 * @return ログイン画面
+	 */
 	@GetMapping()
 	public String view(Model model, LoginForm form) {
 		return "login";
 	}
 	
+	/**
+	 * ログイン認証
+	 * 
+	 * @param model
+	 * @param form
+	 * @return ログイン／マイページ画面
+	 */
 	@PostMapping()
 	public String login(Model model, LoginForm form) {
-		System.out.println(form.toString());
-//		var isCorrectLoginId = form.getLoginId().equals(LOGIN_ID);
-//		var isCorrectPassword = form.getPassword().equals(PASSWORD);
-//		System.out.println(isCorrectLoginId);
-//		System.out.println(isCorrectPassword);
-//		var isCorrectUserAuth = isCorrectLoginId && isCorrectPassword;
+//		var encodedPassword = passwordEncoder.encode(form.getPassword());
+//		System.out.println(encodedPassword.toString());
 		
 		var user = service.searchUserByEmail(form.getEmail());
 		
+//		var isCorrectUserAuth = user.isPresent() && 
+//				form.getPassword().equals(user.get().getPassword());
+		
 		var isCorrectUserAuth = user.isPresent() && 
-				form.getPassword().equals(user.get().getPassword());
+				passwordEncoder.matches(form.getPassword(), user.get().getPassword());
 		
 		if(isCorrectUserAuth) {
 			return "redirect:/mypage";
