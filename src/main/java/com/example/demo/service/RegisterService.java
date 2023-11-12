461278f72cae624477608afe02dc017d97465567
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-11-12 14:29:43 +0900

    [Add] show default Spring Security login page

--- src/main/java/com/example/demo/service/RegisterService.java
+++ src/main/java/com/example/demo/service/RegisterService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import java.util.Optional;
 
 import org.dozer.Mapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class RegisterService {
 	private final Mapper mapper;
 	
 	/** PasswordEncoder */
-	private final PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 	
 	public Optional<User> registerUser(RegisterForm form){
 		var existedUser = repository.findByEmail(form.getEmail());
