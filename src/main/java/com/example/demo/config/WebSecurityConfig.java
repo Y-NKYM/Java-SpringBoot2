219de8e62afe553b78f92d069dc1161acdb5f58f
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-11-13 17:28:50 +0900

    [Add] limit view page by authentication

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -6,16 +6,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.demo.constant.UrlConst;
+
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
+	/** ユーザー名のname属性 */
+	private final String USERNAME_PARAMETER = "email";
+	
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
-		http.formLogin(
-			login -> login.loginPage("/login")
-				.usernameParameter("email")
-				.defaultSuccessUrl("/mypage"));
+		http
+		.authorizeHttpRequests(
+			authorize -> authorize.requestMatchers(UrlConst.NO_AUTH).permitAll()
+				.anyRequest().authenticated())
+		
+		.formLogin(
+			login -> login.loginPage(UrlConst.LOGIN)
+				.usernameParameter(USERNAME_PARAMETER)
+				.defaultSuccessUrl(UrlConst.MYPAGE));
 		
 		return http.build();
 	}
