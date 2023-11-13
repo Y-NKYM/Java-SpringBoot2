703167c87ad2bc17edcd4709843a2f6e1be7934b
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-11-13 22:55:12 +0900

    [Add] Spring Security wrong input error custom message

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,16 +1,34 @@
 package com.example.demo.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.demo.constant.UrlConst;
 
+import lombok.RequiredArgsConstructor;
+
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+	
+	/** パスワードエンコーダー */
+	private final PasswordEncoder passwordEncoder;
+	
+	/** SpringSecurity経由でユーザー情報の取得 */
+	private final UserDetailsService userDetailsService;
+	
+	/** メッセージ取得 */
+	private final MessageSource messageSource;
+	
 	/** ユーザー名のname属性 */
 	private final String USERNAME_PARAMETER = "email";
 	
@@ -29,4 +47,19 @@ public class WebSecurityConfig {
 		
 		return http.build();
 	}
+	
+	/**
+	 * Provider定義
+	 * @return カスタマイズProvider情報
+	 */
+	@Bean
+	AuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		provider.setMessageSource(messageSource);
+		
+		return provider;
+	}
+	
 }
