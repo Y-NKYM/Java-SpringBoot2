74e47681ed04b2720cc78975853af4fe28ee751a
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-11-15 17:47:58 +0900

    [Add] account locked/disabled related column variables and methods added

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,16 +1,22 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="user")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	@Id  //Primary keyに必要
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +24,25 @@ public class User {
 	private Integer loginId;
 	private String email;
 	private String password;
+	
+	@Column(name="is_disabled")
+	private boolean isDisabled;
+	
+	@Column(name="login_failure_count")
+	private int loginFailureCount = 0;
+	
+	@Column(name="account_locked_time")
+	private LocalDateTime accountLockedTime;
+	
+	public User incrementLoginFailureCount() {
+		return new User(loginId, email, password, isDisabled, ++loginFailureCount, accountLockedTime);
+	}
+	
+	public User resetLoginFailureInfo() {
+		return new User(loginId, email, password, isDisabled, 0, null);
+	}
+	
+	public User updateAccountLocked() {
+		return new User(loginId, email, password, isDisabled, 0, LocalDateTime.now());
+	}
 }
