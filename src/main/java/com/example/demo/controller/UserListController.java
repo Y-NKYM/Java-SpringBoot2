27900920d5a998a834ea1a9a3b751f632b8b50df
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-11-24 20:31:17 +0900

    [Add] Post mapping added

--- src/main/java/com/example/demo/controller/UserListController.java
+++ src/main/java/com/example/demo/controller/UserListController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.constant.AuthorityKind;
 import com.example.demo.constant.UrlConst;
@@ -21,10 +22,10 @@ public class UserListController {
 	private static final String KEY_USERLIST="userList";
 	
 	/** モデルキー：ユーザー状態Enum */
-	private static final String KEY_USER_STATUS_KINDS="userStatusKinds";
+	private static final String KEY_USER_STATUS_KIND_OPTIONS="userStatusKindOptions";
 	
 	/** モデルキー：ユーザー権限Enum */
-	private static final String KEY_AUTHORITY_KINDS="authorityKinds";
+	private static final String KEY_AUTHORITY_KIND_OPTIONS="authorityKindOptions";
 
 	
 	@GetMapping(UrlConst.USER_LIST)
@@ -32,9 +33,19 @@ public class UserListController {
 		var users = service.editUserList();
 		model.addAttribute(KEY_USERLIST, users);
 		
-		model.addAttribute(KEY_USER_STATUS_KINDS, UserStatusKind.values());
-		model.addAttribute(KEY_AUTHORITY_KINDS, AuthorityKind.values());
+		model.addAttribute(KEY_USER_STATUS_KIND_OPTIONS, UserStatusKind.values());
+		model.addAttribute(KEY_AUTHORITY_KIND_OPTIONS, AuthorityKind.values());
 		
+		return "userList";
+	}
+	
+	@PostMapping(UrlConst.USER_LIST)
+	public String searchUsers(Model model, UserListForm form) {
+		var users = service.editUserListByParam(form);
+		model.addAttribute(KEY_USERLIST, users);
+		
+		model.addAttribute(KEY_USER_STATUS_KIND_OPTIONS, UserStatusKind.values());
+		model.addAttribute(KEY_AUTHORITY_KIND_OPTIONS, AuthorityKind.values());
 		return "userList";
 	}
 	
