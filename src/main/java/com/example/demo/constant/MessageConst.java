010b83f6376ebdaa1b19103c8a87ae3f6898b816
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-11-27 22:11:17 +0900

    [Add] delete user succeess/error message added

--- src/main/java/com/example/demo/constant/MessageConst.java
+++ src/main/java/com/example/demo/constant/MessageConst.java
@@ -2,10 +2,16 @@ package com.example.demo.constant;
 
 /** エラーメッセージID定数クラス */
 public class MessageConst {
-	/** ログイン画面L入力内容誤り */
+	/** ログイン画面 */
 	public static final String LOGIN_WRONG_INPUT = "login.wrongInput";
 	public static final String LOGIN_SUCCEED = "login.succeed";
+	
+	/** 新規登録画面 */
 	public static final String REGISTER_EXISTED_USER = "register.existedUser";
 	public static final String REGISTER_SUCCEED="register.succeed";
 	public static final String FORM_ERROR = "common.formError";
+	
+	/** ユーザー削除 */
+	public static final String USERLIST_DELETE_SUCCEED = "userList.deleteSucceed";
+	public static final String USERLIST_NON_EXISTED_LOGIN_ID = "userList.nonExistedLoginId";
 }
